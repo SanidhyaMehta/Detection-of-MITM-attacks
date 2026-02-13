d81ca2dfdb5d6fb6bbffffda36d9e7727cbe15e1
Sanidhya <sanidhyamehta1617@gmail.com>
2026-02-13 20:41:59 +0530

    update the libraries

--- tests/test_ml.py
+++ tests/test_ml.py
@@ -2,6 +2,8 @@
 import os
 import joblib
 
+## imported some more libraries
+
 def test_model_exists():
     """Check if trained ML model exists."""
     assert os.path.isfile("models/mitm_detector.pkl"), "Trained model is missing"
